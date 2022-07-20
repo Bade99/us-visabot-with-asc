41be3ef8e94ddd4775fba44e095ca589358e9648
Daniel Rodríguez <danny@ibm.com>
2022-07-20 13:44:24 -0500

    mínimo 4 días desde hoy

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ const startProcess = async () => {
     const appointmentDates = await getCurrentAppointmentDate(page);
     await goToRescheduleAppointment(page);
     const earlierDay = await getEarlierSpot(page);
-    const isEarlier = new Date(earlierDay) < appointmentDates.consularAppointment && new Date(earlierDay) > new Date().setDate(new Date().getDate() + 3);
-    console.log(new Date().setDate(new Date().getDate() + 3))
+    const isEarlier = new Date(earlierDay) < appointmentDates.consularAppointment && new Date(earlierDay) >= new Date().setDate(new Date().getDate() + 4);
 
     logger.updateLog(`${new Date().toISOString()}: Current date: ${appointmentDates.consularAppointment.toDateString()}, earlier spot: ${earlierDay}. earlier: ${isEarlier}`);
 
