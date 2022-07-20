73b02342790ca50d68ff34c281150c33fb3a3b1b
Daniel Rodríguez <danny@ibm.com>
2022-07-19 23:27:32 -0500

    set status if running or not

--- src/secondsUntilWakeup.js
+++ src/secondsUntilWakeup.js
@@ -1,6 +1,7 @@
 const secondsUntilWakeup = () => {
   const wakeupTime = 13;
   const currentTime = new Date().getHours();
+  console.log(currentTime)
   return (wakeupTime - currentTime) * 60 * 60;
 }
 
